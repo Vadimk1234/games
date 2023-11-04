2dcfb2f841209a0eb8ceb582d640fc7b7bba7c41
Vadimk1234 <120939972+Vadimk1234@users.noreply.github.com>
2023-11-04 11:29:11 +0200

    Add files via upload

--- from PyQt5.py
+++ from PyQt5.py
@@ -1,23 +1,93 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import*
-from random import*
-app=QApplication([])
-win=QWidget()
-win.setWindownTitle("Win picker")
-win.move(100,100)
-win.resize(400,200)
-button=QPushButton("Generate")
-text=QLabel("Press and get know winner")
-winer=QLabel("")
-line=QHBoxLayout()
-line.addWidget(text,alignment=Qt.AlignCenter)
-line.addWidget(winer,alignment=Qt.AlignCenter)
-line.addWidget(button,alignment=Qt.AlignCenter)
-win.setLayout(line)
-def show():
-    number=randint(0,99)
-    winer.setText(str(number))
-    text.setText("winner")
-button.clicked.connect(show)
-win.show()
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWidget, QLineEdit, QTextEdit, QInputDialog, QHBoxLayout, QVBoxLayout, QFormLayout
+
+import json
+
+app = QApplication([])
+
+
+notes = {
+    "Good day its smart NOTES":{
+        "text":"Best app",
+        "tegs":["max","oleg"]
+    }
+}
+with open ("notes.json","w") as file :
+    json.dump(notes,file)
+
+
+# параметри вікна програми
+notes_win = QWidget()
+notes_win.setWindowTitle('Розумні замітки')
+notes_win.resize(900, 600)
+
+# віджети вікна програми
+list_notes = QListWidget()
+list_notes_label = QLabel('Список заміток')
+
+
+button_note_create = QPushButton('Створити замітку') # з'являється вікно з полем "Введіть ім'я замітки"
+button_note_del = QPushButton('Видалити замітку')
+button_note_save = QPushButton('Зберегти замітку')
+
+
+field_tag = QLineEdit('')
+field_tag.setPlaceholderText('Введіть тег...')
+field_text = QTextEdit()
+button_tag_add = QPushButton('Додати до замітки')
+button_tag_del = QPushButton('Відкріпити від замітки')
+button_tag_search = QPushButton('Шукати замітки по тегу')
+list_tags = QListWidget()
+list_tags_label = QLabel('Список тегів')
+
+
+# розташування віджетів по лейаутах
+layout_notes = QHBoxLayout()
+col_1 = QVBoxLayout()
+col_1.addWidget(field_text)
+
+
+col_2 = QVBoxLayout()
+col_2.addWidget(list_notes_label)
+col_2.addWidget(list_notes)
+row_1 = QHBoxLayout()
+row_1.addWidget(button_note_create)
+row_1.addWidget(button_note_del)
+row_2 = QHBoxLayout()
+row_2.addWidget(button_note_save)
+col_2.addLayout(row_1)
+col_2.addLayout(row_2)
+
+
+col_2.addWidget(list_tags_label)
+col_2.addWidget(list_tags)
+col_2.addWidget(field_tag)
+row_3 = QHBoxLayout()
+row_3.addWidget(button_tag_add)
+row_3.addWidget(button_tag_del)
+row_4 = QHBoxLayout()
+row_4.addWidget(button_tag_search)
+
+col_2.addLayout(row_3)
+col_2.addLayout(row_4)
+
+layout_notes.addLayout(col_1, stretch = 2)
+layout_notes.addLayout(col_2, stretch = 1)
+notes_win.setLayout(layout_notes)
+
+def show_note():
+    key = list_notes.selectedItems()[0].text()
+    print(key)
+    field_text.setText(notes[key]["text"])
+    list_tags.clear()
+    list_tags.addItem(notes[key]["tags"])
+
+notes.itemCliked.connect(show_note)
+
+# запуск програми
+notes_win.show()
+with open ("notes_data.json","r") as file :
+    notes = json.load(file)
+list_notes.addItems(notes)
+
 app.exec_()
