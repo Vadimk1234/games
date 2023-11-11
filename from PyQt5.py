54c21833738c5bb2f473d93ba9b7b33210055367
Vadimk1234 <120939972+Vadimk1234@users.noreply.github.com>
2023-11-11 16:02:08 +0200

    Update from PyQt5.py

--- from PyQt5.py
+++ from PyQt5.py
@@ -82,6 +82,80 @@ def show_note():
     list_tags.clear()
     list_tags.addItem(notes[key]["tags"])
 
+def save_notes(): 
+    if list_notes.selectedItems(): 
+      key = list_notes.selectedItems()[0].text() 
+      notes[key]["текст"] = field_tag.toPlainText() 
+      with open("notes_data.json", "w") as file: 
+        json.dump(notes,file,sort_keys=True,ensure_ascii=False)     
+        print(notes) 
+    else: 
+       print("ти не зберигаеш ") 
+def del_notes(): 
+    if list_notes.selectedItems(): 
+        key = list_notes.selectedItems()[0].text() 
+        del notes[key] 
+        list_notes.clear() 
+        list_tags.clear() 
+        field_text.clear() 
+        list_notes.addItems(notes) 
+        with open("notes_data.json", "w") as file: 
+            json.dump(notes,file,sort_keys=True,ensure_ascii=False)     
+        print(notes) 
+    else: 
+        print("ти не зберигаеш ") 
+ 
+def add_tags(): 
+    if list_notes.selectedItems(): 
+        key = list_notes.selectedItems()[0].text() 
+        tag = field_tag.text() 
+        if not tag in notes[key]["тегс"]: 
+            notes[key]["теги"].append(tag) 
+            list_tags.addItems(tag) 
+            field_tag.clear() 
+            with open("notes_data.json", "w") as file: 
+                json.dump(notes,file,sort_keys=True,ensure_ascii=False)     
+            print(notes) 
+    else: 
+            print("ти не зберигаеш ") 
+ 
+def del_tags(): 
+    if list_notes.selectedItems(): 
+        key = list_notes.selectedItems()[0].text() 
+        tag = field_tag.text() 
+        notes[key]["тегс"].remove(tag) 
+        list_tags.clear() 
+        list_tags.addItems(notes[key]["теги"]) 
+        with open("notes_data.json", "w") as file: 
+            json.dump(notes,file,sort_keys=True,ensure_ascii=False)     
+        print(notes) 
+    else: 
+        print("ти ніц не вибрал ніц для видалення тегів") 
+         
+def search_tags(): 
+    print(button_tag_search.text()) 
+    tag = field_tag 
+    if button_tag_search.text() == " шукати замітки по тегу" and tag: 
+        print(tag) 
+        notes_filtred = {} 
+        for note in notes: 
+            if tag in notes[note]["теги"]: 
+                notes_filtred[note]==notes[note] 
+        button_note_create.setText("Скинути") 
+        list_notes.clear() 
+        list_tags.clear() 
+        list_notes.addItems(notes_filtred) 
+        print(button_tag_search.text()) 
+    elif button_tag_search.text() == "скинути замітку": 
+        list_notes.clear() 
+        list_tags.clear() 
+        field_tag.clear() 
+        list_notes.addItems(notes) 
+        button_tag_search.setText("шукати замітки по тегу") 
+        print(notes) 
+    else: 
+        pass
+
 notes.itemCliked.connect(show_note)
 
 # запуск програми
